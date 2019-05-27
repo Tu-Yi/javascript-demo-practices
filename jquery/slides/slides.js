48211b5f3e417ad87ee7129f21e057df1e507bc6
kun.yuan <yk_niliv@qq.com>
2019-05-27 17:26:53 +0800

    to add on slides

--- jquery/slides/slides.js
+++ jquery/slides/slides.js
@@ -1,43 +1,50 @@
-setTimeout(() => {
-    $('.images > img:nth-child(1)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images > img:nth-child(2)').css({
-        transform: 'translateX(-100%)'
-    })
 
-    $('.images > img:nth-child(1)').one('transitionend',function(e){
-        $(e.currentTarget).addClass('right').css({
-            transform: 'none'
-        })
+let n;
+/*初始化状态 */
+init();
+setInterval(() => {
+    /**图片状态切换 */
+    makeLeave(getImage(n)).one('transitionend', (e)=>{
+        makeEnter($(e.currentTarget))
     })
-
+    makeCurrent(getImage(n+1))
+    n+=1;
 }, 3000);
 
-setTimeout(() => {
-    $('.images > img:nth-child(2)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images > img:nth-child(3)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images > img:nth-child(2)').one('transitionend',function(e){
-        $(e.currentTarget).addClass('right').css({
-            transform: 'none'
-        })
-    })
-}, 6000);
 
-setTimeout(() => {
-    $('.images > img:nth-child(3)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images > img:nth-child(1)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images > img:nth-child(3)').one('transitionend',function(e){
-        $(e.currentTarget).addClass('right').css({
-            transform: 'none'
-        })
-    })
-}, 9000);
+
+
+
+
+/**封装函数 */
+
+function getImage(n){
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n){
+    if(n>3){
+        n = n%3;
+        if(n===0){
+            n=3
+        }
+    }
+    return n;
+}
+function init(){
+    n=1;
+    $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+    
+    return $node.removeClass('enter current').addClass('leave');
+}
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
+}
+
