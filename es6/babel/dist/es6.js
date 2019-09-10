b3d09add49c81dbaea80fc5c0391e83091d39155
kun.yuan <yk_niliv@qq.com>
2019-09-10 19:18:59 +0800

    to add on

--- es6/babel/dist/es6.js
+++ es6/babel/dist/es6.js
@@ -1,4 +1,10 @@
-"use strict";
+'use strict';
+
+var _user = require('./user');
+
+var _user2 = _interopRequireDefault(_user);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var a = 1;
 var b = 2,
@@ -7,6 +13,8 @@ var b = 2,
 var d = 2;
 
 var arr = function arr() {
-    alert(a + b + c);
+  alert(a + b + c);
 };
 arr();
+
+var user = new _user2.default('niliv');
---
+++ es6/babel/dist/user.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+function User(name) {
+  this.name = name;
+}
+
+exports.default = User;
